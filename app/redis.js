f5ea2c6b0e94a26b70180b808c946911952c79ad
Joaquin Furlan <jfurlan1@globalthink.io>
2025-07-11 16:52:02 -0300

    Actualizo a 3 replicas

--- app/redis.js
+++ app/redis.js
@@ -8,12 +8,14 @@ const client = redis.createClient({
 
 client.connect()
   .then(() => {
+    console.log("Conexión a Redis exitosa");
     return client.set("hello", "world");
   })
   .then(() => {
+    console.log("Se escribió en Redis: hello=world");
     return client.get("hello");
   })
   .then(value => {
-    console.log("Redis value:", value);
+    console.log("Redis value: ", value); // Esto debería mostrar "world"
   })
-  .catch(err => console.error("Redis error", err));
+  .catch(err => console.error("Redis error: ", err));
