65d213aa7d0519eeb6dcbc65a440901fd112d554
Joaquin Furlan <jfurlan1@globalthink.io>
2025-07-13 12:34:08 -0300

    tls=true en redis.js

--- app/redis.js
+++ app/redis.js
@@ -6,16 +6,22 @@ const port = 80;
 const client = redis.createClient({
   socket: {
     host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
+    port: process.env.REDIS_PORT,
+    tls: true // 🟢 CLAVE: Habilita conexión segura con ElastiCache
   }
 });
 
 client.connect().catch(console.error);
 
 app.get("/", async (req, res) => {
-  await client.set("hello", "world");
-  const value = await client.get("hello");
-  res.send(`Hello from Redis: ${value}`);
+  try {
+    await client.set("hello", "world");
+    const value = await client.get("hello");
+    res.send(`Hello from Redis: ${value}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Redis error");
+  }
 });
 
 app.listen(port, () => {
